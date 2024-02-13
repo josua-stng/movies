16d3923d8af4e69d724eb31c6feea953a5236370
josua-stng <josuasitanggang51@gmail.com>
2024-02-13 19:50:41 +0700

    refactor: trending movie component

--- src/app/movies/trending-movies/trending-movies.tsx
+++ src/app/movies/trending-movies/trending-movies.tsx
@@ -3,7 +3,6 @@ import { CarouselMovies } from './carousel-movies';
 export default function TrendingMovies() {
   return (
     <div>
-      <p>Trending Movies</p>
       <CarouselMovies />
     </div>
   );
