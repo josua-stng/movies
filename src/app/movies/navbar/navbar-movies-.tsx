200124026e26d36475c267c5dbee3cac52475cf1
josua-stng <josuasitanggang51@gmail.com>
2024-02-16 19:40:53 +0700

    feat: link href to home page

--- src/app/movies/navbar/navbar-movies-.tsx
+++ src/app/movies/navbar/navbar-movies-.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import SearchMovies from './search-movies';
 
 export default function NavbarMovies() {
   return (
-    <nav className="flex justify-between items-center p-3 pb-4 bg-sky-800    ">
+    <nav className="flex justify-between items-center p-3 pb-4 bg-sky-900   sticky top-0 z-10">
       <div className="flex space-x-5 text-slate-100 font-semibold">
-        <h2>Movies</h2>
+        <Link href={'/'}>Movies</Link>
         <h2>Popular</h2>
         <h2>Top</h2>
         <h2>Upcoming</h2>
