1ae94d7e1503609e97946169babad53b48f9535f
josua-stng <josuasitanggang51@gmail.com>
2024-02-16 19:41:13 +0700

    refactor: code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import NavbarMovies from './movies/navbar/navbar-movies-';
 import TrendingMovies from './movies/trending-movies/trending-movies';
 export default function Home() {
   return (
     <>
-      <NavbarMovies />
       <TrendingMovies />
     </>
   );
