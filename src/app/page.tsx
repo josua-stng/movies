f22c6670cd178041416b17074f415284b80b3b0a
josua-stng <josuasitanggang51@gmail.com>
2024-02-19 20:31:16 +0700

    feat: add popular movies component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import TrendingMovies from './movies/trending-movies/trending-movies';
+import PopularMovies from './popular-movies/popular-movies';
 export default function Home() {
   return (
     <>
       <TrendingMovies />
+      <PopularMovies />
     </>
   );
 }
