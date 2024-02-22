e8f17eba0d933c40ceddf3d867c506b0bdc6824b
josua-stng <josuasitanggang51@gmail.com>
2024-02-22 19:16:28 +0700

    feat: add carousel,layout,& page component

---
+++ src/app/top-movies/carousel-top-movies.tsx
@@ -0,0 +1,57 @@
+'use client';
+import useTopMovies from '../query/movies/use-fetch-top-movies';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import Image from 'next/image';
+import Link from 'next/link';
+import { SkeletonCarouselMovies } from '../lib/skeleton-carousel-movies';
+import { PropsCarouselTopMovies } from '../lib/types';
+export default function CarouselTopMovies() {
+  const { data: movies, isPending } = useTopMovies();
+  if (isPending) {
+    return <SkeletonCarouselMovies />;
+  }
+  return (
+    <Carousel className="w-full max-w-7xl mx-auto">
+      <h1 className="font-bold ml-2 text-xl italic mt-3 mb-1">Top Movies</h1>
+      <CarouselContent className="ml-1">
+        {movies?.map((movie: PropsCarouselTopMovies) => {
+          return (
+            <CarouselItem
+              key={movie.id}
+              className="pl-1 md:basis-1/4 lg:basis-1/6"
+            >
+              <Link href={`/movies/${movie.id}`} className="">
+                <Card className="hover:bg-slate-200 cursor-pointer">
+                  <CardContent className="flex aspect-square items-center justify-center p-2">
+                    <Image
+                      src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                      alt="image-movie"
+                      width={500}
+                      height={200}
+                      className="w-full h-full object-cover rounded-md"
+                    />
+                  </CardContent>
+                  <div className=" ml-3 mt-1 space-y-1 mb-2">
+                    <p className="line-clamp-1 font-bold text-sm">
+                      {movie.original_title}
+                    </p>
+                    <p className="text-xs">{movie.release_date}</p>
+                  </div>
+                </Card>
+              </Link>
+            </CarouselItem>
+          );
+        })}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
---
+++ src/app/top-movies/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+import useTopMovies from '../query/movies/use-fetch-top-movies';
+import SkeletonPopularMovie from '../lib/skeleton-popular-movie';
+import LayoutTopMovies from './layout-top-movies';
+import { PropsTopMovies } from '../lib/types';
+import 'animate.css';
+export default function TopMovies() {
+  const { data: movies, isPending } = useTopMovies();
+  if (isPending) {
+    return <SkeletonPopularMovie />;
+  }
+  return (
+    <div className="p-5 mt-5 animate__animated animate__fadeIn">
+      <h1 className="font-bold text-xl mb-2 italic font-sans">Top Movies</h1>
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 max-w-8xl gap-5 mx-auto justify-center">
+        {movies?.map((movie: PropsTopMovies) => {
+          return (
+            <LayoutTopMovies
+              key={movie.id}
+              id={movie.id}
+              image={movie.backdrop_path}
+              original_title={movie.original_title}
+              popularity={movie.popularity}
+              release_date={movie.release_date}
+              overview={movie.overview}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
