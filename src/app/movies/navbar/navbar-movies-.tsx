e96f0585223814088c566396d25b649388d16bc0
josua-stng <josuasitanggang51@gmail.com>
2024-02-22 19:13:08 +0700

    feat: add link for popular,top & upcoming page

--- src/app/movies/navbar/navbar-movies-.tsx
+++ src/app/movies/navbar/navbar-movies-.tsx
@@ -3,12 +3,20 @@ import SearchMovies from './search-movies';
 
 export default function NavbarMovies() {
   return (
-    <nav className="flex justify-between items-center p-3 pb-4 bg-sky-900   sticky top-0 z-10">
+    <nav className="flex justify-between items-center p-3 pb-4 bg-sky-900 sticky top-0 z-10">
       <div className="flex space-x-5 text-slate-100 font-semibold">
-        <Link href={'/'}>Movies</Link>
-        <h2>Popular</h2>
-        <h2>Top</h2>
-        <h2>Upcoming</h2>
+        <Link href={'/'} className="hover:underline">
+          Movies
+        </Link>
+        <Link href={'/popular-movies'} className="hover:underline ">
+          Popular
+        </Link>
+        <Link href={'/top-movies'} className="hover:underline">
+          Top
+        </Link>
+        <Link href={'/upcoming-movies'} className="hover:underline">
+          Upcoming
+        </Link>
       </div>
       <div>
         <SearchMovies />
