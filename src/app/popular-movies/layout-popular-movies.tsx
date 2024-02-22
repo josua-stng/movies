23b6378cf1385cce235d5cbea4eb969578e158c8
josua-stng <josuasitanggang51@gmail.com>
2024-02-22 19:07:08 +0700

    feat: add layout popular movies

---
+++ src/app/popular-movies/layout-popular-movies.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import { MdOutlineStarBorder } from 'react-icons/md';
+import Link from 'next/link';
+import { PropsLayoutPopularMovies } from '../lib/types';
+export default function LayoutPopularMovies({
+  id,
+  image,
+  original_title,
+  popularity,
+  release_date,
+  overview,
+}: PropsLayoutPopularMovies) {
+  return (
+    <Link
+      href={`/movies/${id}`}
+      className="border border-slate-300 mx-auto justify-center p-3 space-y-2 rounded-md hover:bg-slate-100 cursor-pointer"
+    >
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${image}`}
+        width={400}
+        height={400}
+        className="object-cover rounded-md"
+        alt="image-movie"
+      />
+      <div className="flex justify-between items-center">
+        <h1 className="font-bold ">{original_title}</h1>
+        <div className="flex items-center">
+          <MdOutlineStarBorder />
+          <p className="text-sm">{Math.floor(popularity)}</p>
+        </div>
+      </div>
+      <p className="text-xs">{release_date}</p>
+      <p className="line-clamp-3 text-sm font-base mt-2">{overview}</p>
+    </Link>
+  );
+}
