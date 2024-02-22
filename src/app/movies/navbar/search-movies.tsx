a76561bc11362666470d41fd838590aaa9d54b5c
josua-stng <josuasitanggang51@gmail.com>
2024-02-22 19:13:46 +0700

    fix: search params when query null

--- src/app/movies/navbar/search-movies.tsx
+++ src/app/movies/navbar/search-movies.tsx
@@ -1,10 +1,27 @@
+'use client';
 import { Input } from '@/components/ui/input';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
 export default function SearchMovies() {
+  const [searchParams, setSearchParams] = useState('');
+  const router = useRouter();
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (searchParams === '') {
+      router.push('/');
+      return;
+    }
+    router.push(`/search?query=${searchParams}`);
+  };
   return (
-    <Input
-      placeholder="search movies"
-      type="text"
-      className="placeholder:italic"
-    />
+    <form onSubmit={handleSubmit}>
+      <Input
+        value={searchParams}
+        onChange={(e) => setSearchParams(e.target.value)}
+        placeholder="search movies"
+        type="text"
+        className="placeholder:italic"
+      />
+    </form>
   );
 }
