d9836f324c3db1fa0f7c01a0b92e4e16355c7d4a
josua-stng <josuasitanggang51@gmail.com>
2024-02-23 20:31:18 +0700

    fix: top movies component

--- src/app/top-movies/page.tsx
+++ src/app/top-movies/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 import useTopMovies from '../query/movies/use-fetch-top-movies';
 import SkeletonPopularMovie from '../lib/skeleton-popular-movie';
-import LayoutTopMovies from './layout-top-movies';
-import { PropsTopMovies } from '../lib/types';
+import { TopMovies } from '../lib/types';
 import 'animate.css';
+import TopMovie from './top-movie';
 export default function TopMovies() {
   const { data: movies, isPending } = useTopMovies();
   if (isPending) {
@@ -13,9 +13,9 @@ export default function TopMovies() {
     <div className="p-5 mt-5 animate__animated animate__fadeIn">
       <h1 className="font-bold text-xl mb-2 italic font-sans">Top Movies</h1>
       <div className="grid md:grid-cols-3 lg:grid-cols-4 max-w-8xl gap-5 mx-auto justify-center">
-        {movies?.map((movie: PropsTopMovies) => {
+        {movies?.map((movie: TopMovies) => {
           return (
-            <LayoutTopMovies
+            <TopMovie
               key={movie.id}
               id={movie.id}
               image={movie.backdrop_path}
